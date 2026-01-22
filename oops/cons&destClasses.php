508832a6e276ad4870c4cb64df335179a35f8e5c
Toheed Ahmad <toheedswati786@gmail.com>
2026-01-22 21:04:26 +0500

    construct and destruct clases

--- oops/cons&destClasses.php
+++ oops/cons&destClasses.php
@@ -6,10 +6,19 @@ class person{
     {
         $arraySize = sizeof($data);
         for($i = 0; $i< $arraySize; $i++){
-            echo $this->name = $data[$i];
+            echo $data[$i];
             echo PHP_EOL;
         }
         return $this;
+        }
+
+        public function __destruct(array $data)
+        {
+            $arraySize = sizeof($data);
+            for($i = 0; $i< $arraySize; $i++){
+                $data[$i] = null;
+            }
+            echo "Everything clearred";
     }
 
 }
